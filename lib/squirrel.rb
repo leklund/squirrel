aa3ebbd46dbf7e98e17a4c45de8cb6b1b356b805
Lukas Eklund <leklund@mediamatters.org>
2009-01-08 19:43:34 -0500

    add contains_i to allow for case-insenstive queries (useful with postgresql)

--- lib/squirrel.rb
+++ lib/squirrel.rb
@@ -503,6 +503,7 @@ module Squirrel
           else                   [ "=",       arg_format,        values ]
           end
         when :contains   then    [ "LIKE",    arg_format,        values.map{|v| "%#{v}%" } ]
+        when :contains_i then    [ "ILIKE",    arg_format,        values.map{|v| "%#{v}%" } ]
         else
           case operand
           when Condition then    [ op,        oprand.full_name,  [] ] 
